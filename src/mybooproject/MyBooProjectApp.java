a6284b105cb4f57252253da1bbb83743ef890bf1
Renaud Jenny <renaud.jenny@gmail.com>
2012-04-03 14:55:31 +0200

    Initial commit

---
+++ src/mybooproject/MyBooProjectApp.java
@@ -0,0 +1,44 @@
+/*
+ * MyBooProjectApp.java
+ */
+
+package mybooproject;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class MyBooProjectApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new MyBooProjectView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of MyBooProjectApp
+     */
+    public static MyBooProjectApp getApplication() {
+        return Application.getInstance(MyBooProjectApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(MyBooProjectApp.class, args);
+    }
+}
